74d2e92d2b85921b8b6eba65ee8adb1b9a79de09
Danimae Janssen <janssenda@gmail.com>
2018-05-07 13:48:34 -0500

    x

--- Tests/Test_Deck_Configurator-test.py
+++ Tests/Test_Deck_Configurator-test.py
@@ -3,8 +3,6 @@ import unittest
 import DeckConfigurator
 from mock.mock import patch
 
-
-
 class Test_Deck_Configurator(unittest.TestCase):
 
     @patch('six.moves.builtins.open')
@@ -36,7 +34,6 @@ class Test_Deck_Configurator(unittest.TestCase):
                                            'Clubs': 'Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King'},
                                       'Card_Values': {'Ace': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
                                                       '8': 8, '9': 9, '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10}}
-
         cfg.read_yml("test")
 
         assert cfg.card_dict == control_card_dict
